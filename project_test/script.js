010ceb2a830591021276e166e995951558cbdc63
Muhammad Farid <mfarid@Muhammads-MacBook-Pro.local>
2024-01-16 20:17:36 +0800

    kinda manage the zooming in n out part

--- project_test/script.js
+++ project_test/script.js
@@ -1,11 +1,29 @@
-const scrollingImages = document.querySelectorAll(".scrolling-image");
+const images = document.querySelectorAll(".scrolling-image");
 
 window.addEventListener("scroll", () => {
 	const scrollPosition = window.scrollY;
+	const firstImage = images[0];
+	const secondImage = images[1];
 
-	scrollingImages.forEach((image, index) => {
-		const speed = 0.5 * (index + 1);
-		const translateX = -scrollPosition * speed + "px";
-		image.style.transform = `translateX(${translateX})`;
-	});
+	const translateY = (scrollPosition - firstImage.offsetTop) * 0.01 + "px";
+	const scale = 1 + (scrollPosition - firstImage.offsetTop) / 200;
+
+	firstImage.style.transform = `translateY(${translateY}) scale(${scale})`;
+
+	if (scrollPosition > firstImage.offsetTop + firstImage.offsetHeight) {
+		const opacity =
+			1 -
+			(scrollPosition -
+				(firstImage.offsetTop + firstImage.offsetHeight)) /
+				500;
+
+		firstImage.style.opacity = opacity < 0 ? 0 : opacity;
+		secondImage.style.opacity =
+			(scrollPosition -
+				(firstImage.offsetTop + firstImage.offsetHeight)) /
+			500;
+	} else {
+		firstImage.style.opacity = 1;
+		secondImage.style.opacity = 0;
+	}
 });
