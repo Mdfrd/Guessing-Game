6e49b602fe343fa9e75860a231e21d23e3a59dd4
Muhammad Farid <mfarid@Muhammads-MacBook-Pro.local>
2024-01-16 19:14:46 +0800

    added js, for animation with scroll

--- project_test/script.js
+++ project_test/script.js
@@ -1,11 +1,11 @@
-// /*Change background (I need it in this way)*/
-// $("#first").css({ background: "none" });
+const scrollingImages = document.querySelectorAll(".scrolling-image");
 
-// /*Scrolling*/
-// $(window).scroll(function () {
-// 	var $maxScroll = 500;
-// 	var $maxScale = 1.3;
-// 	var $x = $(window).scrollTop() / 100 + 1;
-// 	if ($(window).scrollTop() > $maxScroll) $x = $maxScale;
-// 	$("#first .background").css({ transform: "scale(" + $x + "," + $x + ")" });
-// });
+window.addEventListener("scroll", () => {
+	const scrollPosition = window.scrollY;
+
+	scrollingImages.forEach((image, index) => {
+		const speed = 0.5 * (index + 1);
+		const translateX = -scrollPosition * speed + "px";
+		image.style.transform = `translateX(${translateX})`;
+	});
+});
