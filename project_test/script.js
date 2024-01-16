3fcbd1a61c3c7ccbd9184391a20649d9ae473095
Muhammad Farid <mfarid@Muhammads-MacBook-Pro.local>
2024-01-16 18:54:24 +0800

    kinda added some new css stuff

--- project_test/script.js
+++ project_test/script.js
@@ -1,11 +1,11 @@
-/*Change background (I need it in this way)*/
-$('#first').css({background:'none'});
-$('#first .background').css({backgroundImage: "url('1.jpg')"});
+// /*Change background (I need it in this way)*/
+// $("#first").css({ background: "none" });
 
-/*Scrolling*/
-$(window).scroll(function()
-{   var $maxScroll=300;
-    var $maxScale=1.3;
-    var $x=$(window).scrollTop()/1000+1;
-    if($(window).scrollTop()>$maxScroll) $x=$maxScale;
-    $('#first .background').css({transform: 'scale('+$x+','+$x+')'})
+// /*Scrolling*/
+// $(window).scroll(function () {
+// 	var $maxScroll = 500;
+// 	var $maxScale = 1.3;
+// 	var $x = $(window).scrollTop() / 100 + 1;
+// 	if ($(window).scrollTop() > $maxScroll) $x = $maxScale;
+// 	$("#first .background").css({ transform: "scale(" + $x + "," + $x + ")" });
+// });
